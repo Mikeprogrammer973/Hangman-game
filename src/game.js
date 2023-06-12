01c6b8ba8d4ac7a8edb27f78c574ee647cbf6629
Mikeprogrammer973 <96785933+Mikeprogrammer973@users.noreply.github.com>
2023-06-12 10:26:18 -0400

    2 files modified

--- src/game.js
+++ src/game.js
@@ -12,15 +12,19 @@ let c_word;
 let i_letter;
 let words = ['dog', 'cat', 'response'];
 let i_focused = false;
-let g_h 
+let g_h;
+let c_i = 0;
+let draw
+let intv_piscar
 
 window.onload = () => {
+  draw = SVG().addTo("#c-boneco").size(200, 300)
 
-  if(!localStorage.game_history){
-    localStorage.game_history = JSON.stringify([])
+  if (!localStorage.game_history) {
+    localStorage.game_history = JSON.stringify([]);
   }
 
-  show_history(false)
+  show_history(false);
 
   c_msg = document.getElementById('c-msg');
   c_word = document.getElementById('c-word');
@@ -62,42 +66,96 @@ window.onload = () => {
   });
 };
 
-function update_history(word, stat){
+function update_history(word, stat) {
   g_h.push([word, stat]);
-  localStorage.game_history = JSON.stringify(g_h)
-  show_history(false)
+  localStorage.game_history = JSON.stringify(g_h);
+  show_history(false);
 }
-function show_history(mode){
-  g_h = JSON.parse(localStorage.game_history)
-  
-  let r_qtd = 0
-  document.getElementById("h-w-s").innerHTML = ""
+function show_history(mode) {
+  g_h = JSON.parse(localStorage.game_history);
 
-  g_h.forEach((w)=>{
-    let dv = document.createElement("div")
-    dv.innerHTML = w[0]
-    if(w[1]){
-      dv.style.backgroundColor = "green"
-      r_qtd++
+  let r_qtd = 0;
+  document.getElementById('h-w-s').innerHTML = '';
+
+  g_h.forEach((w) => {
+    let dv = document.createElement('div');
+    dv.innerHTML = w[0];
+    if (w[1]) {
+      dv.style.backgroundColor = 'green';
+      r_qtd++;
     }
-    document.getElementById("h-w-s").append(dv)
-  })
-  document.getElementById("win").innerHTML = "Win: " + r_qtd
-  document.getElementById("lost").innerHTML = "Lost: " + (g_h.length - r_qtd)
-  document.getElementById("total").innerHTML = "Total: " + g_h.length
+    document.getElementById('h-w-s').append(dv);
+  });
+  document.getElementById('win').innerHTML = 'Win: ' + r_qtd;
+  document.getElementById('lost').innerHTML = 'Lost: ' + (g_h.length - r_qtd);
+  document.getElementById('total').innerHTML = 'Total: ' + g_h.length;
 
-  // Without details 
-   if(!mode){
-     document.getElementById("game-history").innerHTML = `${r_qtd}/${g_h.length}`
-     return
-   }
-  // With details 
-   document.getElementById("c-g-h").style.display = "block"
+  // Without details
+  if (!mode) {
+    document.getElementById(
+      'game-history'
+    ).innerHTML = `${r_qtd}/${g_h.length}`;
+    return;
+  }
+  // With details
+  document.getElementById('c-g-h').style.display = 'block';
+}
 
+function piscar() {
+  let color = c_i == 0 ? 'transparent' : 'darkred';
+  if (c_i == 0) {
+    c_i = 1;
+  } else {
+    c_i = 0;
+  }
 }
 
 function hangman(error) {
+  if(error == 1){
+    let p1 = draw.line(5, 190, 190, 290).move(190, 5).stroke({color:'black', width: 10, linecap: 'round'})
+    p1.plot(20, 10, 20, 280)
 
+    let p2 = draw.line(5, 190, 190, 290).move(190, 5).stroke({color:'black', width: 10, linecap: 'round'})
+    p2.plot(20, 10, 100, 10)
+
+    let p3 = draw.line(5, 190, 190, 290).move(190, 5).stroke({color:'black', width: 10, linecap: 'round'})
+    p3.plot(70, 10, 20, 50)
+
+    let p4 = draw.line(5, 190, 190, 290).move(190, 5).stroke({color:'black', width: 10, linecap: 'round'})
+    p4.plot(100, 10, 100, 50)
+
+    let p5 = draw.line(5, 190, 190, 290).move(190, 5).stroke({color:'black', width: 10, linecap: 'round'})
+    p5.plot(10, 285, 100, 285)
+
+    draw.circle(50).fill("red").move(75, 50)
+  }
+  if(error == 2){
+    draw.line(150, 95, 150, 225).move(100, 55).stroke({color:'black', width: 10, linecap: 'round'})
+  }
+  if(error == 3){
+    let bras1 = draw.line(40, 40, 50, 50).move(100, 55).stroke({color:'black', width: 10, linecap: 'round'})
+    bras1.animate(300).plot(100, 125, 120, 165)
+  }
+  if(error == 4){
+    let bras2 = draw.line(40, 40, 50, 50).move(100, 55).stroke({color:'black', width: 10, linecap: 'round'})
+    bras2.animate(300).plot(100, 125, 80, 165)
+  }
+  if(error == 5){
+    let pied1 = draw.line(40, 40, 50, 50).move(100, 55).stroke({color:'black', width: 10, linecap: 'round'})
+    pied1.animate(300).plot(100, 185, 120, 220)
+  }
+  if(error == 6){
+    let pied2 = draw.line(40, 40, 50, 50).move(100, 55).stroke({color:'black', width: 10, linecap: 'round'})
+    pied2.animate(300).plot(100, 185, 80, 220)
+
+    intv_piscar = setInterval(()=>{
+      piscar()
+    }, 300)
+  }
+  if(error == 7){
+    clearInterval(intv_piscar)
+    draw.clear()
+  }
 }
 
 function start_game() {
@@ -150,13 +208,13 @@ function check_letter() {
       c_msg.children[0].innerHTML = 'You win!';
       setTimeout(() => {
         c_msg.style.display = 'none';
-        update_history(word, true)
+        update_history(word, true);
         take_word();
       }, 3000);
     }
   } else {
     errors++;
-    if (errors == bxs_errors.length+1) {
+    if (errors == bxs_errors.length + 1) {
       //alert("You lost!")
       c_msg.style.display = 'flex';
       c_msg.children[0].style.backgroundColor = 'darkred';
@@ -164,7 +222,7 @@ function check_letter() {
         'Game over!<hr><em>The word was: ' + word + '</em>';
       setTimeout(() => {
         c_msg.style.display = 'none';
-        update_history(word, false)
+        update_history(word, false);
         take_word();
       }, 3000);
     } else {
@@ -210,20 +268,20 @@ function take_word() {
     }
     //Dica
     //alert(word)
-    let ld1 = word[0]
-    let ld2 
-    let i = 0
-    while(i < word.length){
-      if(word[i] != ld1){
-        ld2 = word[i]
-        break
+    let ld1 = word[0];
+    let ld2;
+    let i = 0;
+    while (i < word.length) {
+      if (word[i] != ld1) {
+        ld2 = word[i];
+        break;
       }
-      i++
+      i++;
     }
-    i_letter.value = ld1
-    check_letter()
-    i_letter.value = ld2
-    check_letter()
+    i_letter.value = ld1;
+    check_letter();
+    i_letter.value = ld2;
+    check_letter();
     //
     i_letter.focus();
     fill_time_bar();
@@ -235,7 +293,7 @@ function fill_time_bar() {
   if (width >= 1002) {
     width = 0;
     errors++;
-    if (errors == bxs_errors.length+1) {
+    if (errors == bxs_errors.length + 1) {
       //alert("You lost!")
       c_msg.style.display = 'flex';
       c_msg.children[0].style.backgroundColor = 'darkred';
@@ -243,7 +301,7 @@ function fill_time_bar() {
         'Game over!<hr><em>The word was: ' + word + '</em>';
       setTimeout(() => {
         c_msg.style.display = 'none';
-        update_history(word, false)
+        update_history(word, false);
         take_word();
       }, 3000);
     } else {
